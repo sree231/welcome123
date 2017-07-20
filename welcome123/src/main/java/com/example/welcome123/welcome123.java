41ea48ebfb6817a5c638eb0feacb0e5ae627a6fe
sreekanth_way2 <sreekanth.g@way2online.co.in>
2017-07-20 10:32:43 +0530

    welcome123

---
+++ welcome123/src/main/java/com/example/welcome123/welcome123.java
@@ -0,0 +1,11 @@
+package com.example.welcome123;
+
+/**
+ * Created by sys0037 on 20/7/17.
+ */
+
+public class welcome123 {
+    public static void main(String[] args) {
+        System.out.println("Welcome1234");
+    }
+}
